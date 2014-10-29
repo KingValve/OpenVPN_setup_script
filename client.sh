963e57ab93f73efea0b1e020efa79489eb983a8e
KingValve <Sikva.man@gmail.com>
2014-10-29 14:46:52 +0100

    Added USR

--- client.sh
+++ client.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 #
 
+USR="client"
+
 # Functions
 ok() {
     echo -e '\e[32m'$1'\e[m';
@@ -26,14 +28,14 @@ if [[ -z "${SERVER_IP}" ]]; then
 fi
 
 # Generate Client Config
-ok "❯❯❯ Generating Client Config"
-openssl genrsa -out /etc/openvpn/client-key.pem 2048 > /dev/null 2>&1
-chmod 600 /etc/openvpn/client-key.pem
-openssl req -new -key /etc/openvpn/client-key.pem -out /etc/openvpn/client-csr.pem -subj /CN=OpenVPN-Client/ > /dev/null 2>&1
-openssl x509 -req -in /etc/openvpn/client-csr.pem -out /etc/openvpn/client-cert.pem -CA /etc/openvpn/ca.pem -CAkey /etc/openvpn/ca-key.pem -days 36525 > /dev/null 2>&1
+ok "❯❯❯ Generating $USR Config"
+openssl genrsa -out /etc/openvpn/$USR\-key.pem 2048 > /dev/null 2>&1
+chmod 600 /etc/openvpn/$USR\-key.pem
+openssl req -new -key /etc/openvpn/$USR\-key.pem -out /etc/openvpn/$USR\-csr.pem -subj /CN=OpenVPN-$USR/ > /dev/null 2>&1
+openssl x509 -req -in /etc/openvpn/$USR\-csr.pem -out /etc/openvpn/$USR\-cert.pem -CA /etc/openvpn/ca.pem -CAkey /etc/openvpn/ca-key.pem -days 36525 > /dev/null 2>&1
 
-cat > /etc/openvpn/client.ovpn <<EOF
-client
+cat > /etc/openvpn/$USR.ovpn <<EOF
+$USR
 nobind
 dev tun
 redirect-gateway def1 bypass-dhcp
@@ -41,10 +43,10 @@ remote $SERVER_IP 443 tcp
 comp-lzo yes
 
 <key>
-$(cat /etc/openvpn/client-key.pem)
+$(cat /etc/openvpn/$USR\-key.pem)
 </key>
 <cert>
-$(cat /etc/openvpn/client-cert.pem)
+$(cat /etc/openvpn/$USR\-cert.pem)
 </cert>
 <ca>
 $(cat /etc/openvpn/ca.pem)
@@ -54,5 +56,5 @@ EOF
 # Restart Service
 ok "❯❯❯ service openvpn restart"
 service openvpn restart > /dev/null 2>&1
-ok "❯❯❯ Your client config is available at /etc/openvpn/client.ovpn"
+ok "❯❯❯ Your $USR config is available at /etc/openvpn/$USR.ovpn"
 ok "❯❯❯ All done!"
