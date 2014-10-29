35cb965f93b7cfb1c7a3b078a267186381c0efdd
KingValve <Sikva.man@gmail.com>
2014-10-29 16:06:41 +0100

    Working?

--- client.sh
+++ client.sh
@@ -31,11 +31,11 @@ fi
 ok "❯❯❯ Generating $USR Config"
 openssl genrsa -out /etc/openvpn/$USR\-key.pem 2048 > /dev/null 2>&1
 chmod 600 /etc/openvpn/$USR\-key.pem
-openssl req -new -key /etc/openvpn/$USR\-key.pem -out /etc/openvpn/$USR\-csr.pem -subj /CN=OpenVPN-$USR/ > /dev/null 2>&1
-openssl x509 -req -in /etc/openvpn/$USR\-csr.pem -out /etc/openvpn/$USR\-cert.pem -CA /etc/openvpn/ca.pem -CAkey /etc/openvpn/ca-key.pem -days 36525 > /dev/null 2>&1
+openssl req -new -key /etc/openvpn/${USR}-key.pem -out /etc/openvpn/${USR}-csr.pem -subj /CN=OpenVPN-${USR} > /dev/null 2>&1
+openssl x509 -req -in /etc/openvpn/${USR}-csr.pem -out /etc/openvpn/${USR}-cert.pem -CA /etc/openvpn/ca.pem -CAkey /etc/openvpn/ca-key.pem -days 36525 > /dev/null 2>&1
 
 cat > /etc/openvpn/$USR.ovpn <<EOF
-$USR
+${USR}
 nobind
 dev tun
 redirect-gateway def1 bypass-dhcp
@@ -43,10 +43,10 @@ remote $SERVER_IP 443 tcp
 comp-lzo yes
 
 <key>
-$(cat /etc/openvpn/$USR\-key.pem)
+$(cat /etc/openvpn/${USR}-key.pem)
 </key>
 <cert>
-$(cat /etc/openvpn/$USR\-cert.pem)
+$(cat /etc/openvpn/${USR}-cert.pem)
 </cert>
 <ca>
 $(cat /etc/openvpn/ca.pem)
@@ -56,5 +56,5 @@ EOF
 # Restart Service
 ok "❯❯❯ service openvpn restart"
 service openvpn restart > /dev/null 2>&1
-ok "❯❯❯ Your $USR config is available at /etc/openvpn/$USR.ovpn"
+ok "❯❯❯ Your $USR config is available at /etc/openvpn/${USR}.ovpn"
 ok "❯❯❯ All done!"
