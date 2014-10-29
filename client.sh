93450357a02cb8f8854166986321e78563664768
KingValve <Sikva.man@gmail.com>
2014-10-29 09:46:40 +0100

    Added client.sh

---
+++ client.sh
@@ -0,0 +1,24 @@
+ok "❯❯❯ Generating Client Config"
+openssl genrsa -out /etc/openvpn/client-key.pem 2048 > /dev/null 2>&1
+chmod 600 /etc/openvpn/client-key.pem
+openssl req -new -key /etc/openvpn/client-key.pem -out /etc/openvpn/client-csr.pem -subj /CN=OpenVPN-Client/ > /dev/null 2>&1
+openssl x509 -req -in /etc/openvpn/client-csr.pem -out /etc/openvpn/client-cert.pem -CA /etc/openvpn/ca.pem -CAkey /etc/openvpn/ca-key.pem -days 36525 > /dev/null 2>&1
+
+cat > /etc/openvpn/client.ovpn <<EOF
+client
+nobind
+dev tun
+redirect-gateway def1 bypass-dhcp
+remote $SERVER_IP 443 tcp
+comp-lzo yes
+
+<key>
+$(cat /etc/openvpn/client-key.pem)
+</key>
+<cert>
+$(cat /etc/openvpn/client-cert.pem)
+</cert>
+<ca>
+$(cat /etc/openvpn/ca.pem)
+</ca>
+EOF
